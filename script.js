bf33787be703f0df356a246ad3238cc93365345a
AnmolGoyal01 <139466193+AnmolGoyal01@users.noreply.github.com>
2024-06-11 16:16:03 +0530

    Bug fixes and Add Sound

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// variables
 let boxes = document.querySelectorAll(".game button");
 let rst = document.querySelector(".reset");
 let winnerMsg = document.querySelector(".winnerMsg");
 let newGameBtn = document.querySelector(".newGame");
 let turnX = true;
 let counter = 0;
-
-
+let XSound = new Audio("sounds/X.mp3");
+let zeroSound = new Audio("sounds/zero.mp3");
+let WinSound = new Audio("sounds/Win.mp3");
+let resetSound = new Audio("sounds/resetGame.mp3")
 
 const winPattern =
 [
@@ -19,48 +22,21 @@ const winPattern =
     [2, 4, 6],
 ];
 
+// new Game button
 newGameBtn.addEventListener("mouseover", ()=> {
     gsap.to(newGameBtn, {
         scale : 1.1,
         duration : 0.2
     })
 })
-
 newGameBtn.addEventListener("mouseleave" , ()=> {
     gsap.to(newGameBtn, {
         scale : 1,
         duration : .2,
     })
 })
-rst.addEventListener("mouseover", ()=> {
-    gsap.to(rst, {
-        scale : 1.1,
-        duration : 0.2
-    })
-})
-
-rst.addEventListener("mouseleave" , ()=> {
-    gsap.to(rst, {
-        scale : 1,
-        duration : .2,
-    })
-})
-
-rst.addEventListener("click",()=>{
-    gsap.from(rst, {
-        y : 4,
-
-    });
-    document.querySelector(".winner").classList.add("hidden");
-    counter = 0;
-    boxes.forEach(box => {
-        turnX = true;
-        box.innerText = "";
-        box.disabled = false;
-    });
-})
-
 newGameBtn.addEventListener("click",()=>{
+    resetSound.play();
     document.querySelector(".winner").classList.add("hidden");
     counter = 0;
     boxes.forEach(box => {
@@ -70,7 +46,36 @@ newGameBtn.addEventListener("click",()=>{
     });
 })
 
+// reset button
+rst.addEventListener("mouseover", ()=> {
+    gsap.to(rst, {
+        scale : 1.1,
+        duration : 0.2
+    })
+})
+rst.addEventListener("mouseleave" , ()=> {
+    gsap.to(rst, {
+        scale : 1,
+        duration : .2,
+    })
+})
+rst.addEventListener("click",()=>{
+    gsap.from(rst, {
+        y : 4,
+
+    });
+    resetSound.play();
+    document.querySelector(".winner").classList.add("hidden");
+    counter = 0;
+    boxes.forEach(box => {
+        turnX = true;
+        box.innerText = "";
+        box.disabled = false;
+    });
+})
+
 function showWinner(winner) {
+    WinSound.play();
     if (winner !== "draw") {
         let msg =  `Congrats ${winner} wins the Game`;
         winnerMsg.innerText = msg;
@@ -84,8 +89,6 @@ function showWinner(winner) {
         document.querySelector(".winner").classList.remove("hidden");
     }
 }
-
-
 function checkWinner() {
     for (const pattern of winPattern) {
         let p1 = boxes[pattern[0]].innerText;
@@ -98,7 +101,8 @@ function checkWinner() {
         }
     }
 }
-
+ 
+// box animations
 boxes.forEach(box => {
     box.addEventListener("mouseover" , ()=> {
         gsap.to(box, {
@@ -107,7 +111,6 @@ boxes.forEach(box => {
         })
     });
 });
-
 boxes.forEach(box => {
     box.addEventListener("mouseleave" , ()=> {
         gsap.to(box, {
@@ -117,6 +120,8 @@ boxes.forEach(box => {
     });
 });
 
+
+// box click handelling
 boxes.forEach(box => {
     box.addEventListener("click", (event)=>{
         gsap.from(box, {
@@ -127,18 +132,22 @@ boxes.forEach(box => {
             scale : 1,
             duration : 0.1
         })
+
         if (turnX) {
             box.innerText = "X";
+            XSound.play();
             turnX = false;
         }
         else {
             box.innerText = "O";
+            zeroSound.play();
             turnX = true;
         }
         box.disabled = true;
         counter++;
         checkWinner();
         if (counter == 9) {
+            WinSound.play();
             showWinner("draw");
         }
     })
